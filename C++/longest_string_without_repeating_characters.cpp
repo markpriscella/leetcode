4aefee521afcda604b1bb538f40d302d81d93efe
Mark <markpriscella@gmail.com>
2021-03-30 14:21:15 -0400

    added c++ folder and added respective files

---
+++ C++/longest_string_without_repeating_characters.cpp
@@ -0,0 +1,47 @@
+/*
+    Given a string, find the length of the longest substring without repeating characters.
+
+    Example 1:
+
+    Input: "abcabcbb"
+    Output: 3 
+    Explanation: The answer is "abc", with the length of 3.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxSize = s.size();
+        
+        int ans = 0, i = 0, k = 0, j = 0;
+        
+        if(maxSize == 1) {
+            return maxSize;
+        }
+        
+    
+            for(k = 0; k < maxSize; k++) {
+                if(i == k) {
+                    continue;
+                }
+                else if(s[i] == s[k]) {
+                    i++;
+                }
+                else {
+                    for(j = i+1; j < k; j++) {
+                        if(s[j] == s[i]) {
+                            i = j;
+                        }
+                        else if(s[j] == s[k]) {
+                            i = j + 1;
+                        }
+                    }
+                }
+                ans = max(ans,k-i+1);
+            }
+        
+        
+        return ans;
+    }
+    
+};
